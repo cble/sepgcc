4917d272d946a8e9a5a3c2bdb6db893a68692c61
junjie.lin <junjie.lin@dianping.com>
2015-12-29 20:46:16 +0800

    notice

---
+++ src/main/java/com/sepgcc/site/controller/DownloadController.java
@@ -0,0 +1,34 @@
+package com.sepgcc.site.controller;
+
+import com.sepgcc.site.dto.FileMeta;
+import com.sepgcc.site.service.FileService;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class DownloadController extends BaseController {
+
+    private static final Logger log = Logger.getLogger(DownloadController.class);
+
+    @Resource
+    private FileService fileService;
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void download(@RequestParam String fileId, HttpServletResponse response) {
+        try {
+            FileMeta out = fileService.getFile(fileId, 0, true);
+            response.setContentType(out.getFileType());
+            response.setHeader("Content-disposition", "attachment; filename=" + out.getFileName());
+            FileCopyUtils.copy(out.getBytes(), response.getOutputStream());
+        } catch (Exception e) {
+            log.error("download error", e);
+        }
+    }
+}
--- src/main/webapp/WEB-INF/pages/notice.ftl
+++ src/main/webapp/WEB-INF/pages/notice.ftl
@@ -8,7 +8,15 @@
         ${project.description}
     </div>
     <div>
-        <a href="/upload?projectId=${project.id}">开始上传</a>
+        <#if project.projectAttachment?? && project.projectAttachment?size gt 0>
+            <span>下载附件：</span>
+            <#list project.projectAttachment as attachment>
+                <a href="/download?fileId=${attachment.fileId}">${attachment.name}</a>
+            </#list>
+        </#if>
+    </div>
+    <div style="text-align: center;">
+        <a class="btn btn-primary" href="/upload?projectId=${project.id}">开始上传</a>
     </div>
     <hr />
     <div>
