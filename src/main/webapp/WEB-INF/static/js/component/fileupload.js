c0fb5092095683c23042756d7f868fdb5556604a
mangix <maqh1988@gmail.com>
2015-12-29 18:26:48 +0800

    v

--- src/main/webapp/WEB-INF/static/js/component/fileupload.js
+++ src/main/webapp/WEB-INF/static/js/component/fileupload.js
@@ -4,10 +4,10 @@
         container = $(container);
         var id = 'fileupload' + global++;
 
-        container.html('<label class="btn btn-success" for="' + id + '"><input id="' + id + '" type="file" name="files[]" data-url="/uploadfile" style="display:none;" multiple> 添加文件 </label><div class="progress"><div class="progress-bar" style="width: 0%;"></div></div><div class="file_list">');
+        container.html('<label class="btn btn-success" for="' + id + '"><input id="' + id + '" type="file" name="files[]" data-url="/uploadfile" style="display:none;" multiple> 添加文件 </label><div class="loading"></div><div class="file_list">');
         var trigger = container.find("#" + id);
         var list = container.find(".file_list");
-        var bar = container.find(".progress").hide().find(".progress-bar");
+        var loading = container.find(".loading").hide();
         //上传成功的File
         this.files = [];
         this.finish = 0;
@@ -43,7 +43,7 @@
             },
 
             start: function (e) {
-                bar.css('width', '0').parent().show();
+                loading.show();
                 console.log('start', e);
                 self.finish++;
             },
@@ -53,12 +53,12 @@
 
             },
             progressall: function (e, data) {
-                var progress = parseInt(data.loaded / data.total * 100, 10);
-
-                bar.css(
-                    'width',
-                    progress + '%'
-                );
+                //var progress = parseInt(data.loaded / data.total * 100, 10);
+                loading.hide();
+                //bar.css(
+                //    'width',
+                //    progress + '%'
+                //);
             }
         });
 
