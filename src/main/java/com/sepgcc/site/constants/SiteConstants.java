f4f972820932b8b41ee02bc4274aa74c5b59bb82
junjie.lin <junjie.lin@dianping.com>
2016-01-09 10:28:04 +0800

    fix

--- src/main/java/com/sepgcc/site/constants/SiteConstants.java
+++ src/main/java/com/sepgcc/site/constants/SiteConstants.java
@@ -8,14 +8,14 @@ public class SiteConstants {
 
     public static final int PAGE_SIZE = 10;
 
-    public static final String FILE_BASE = "/data/appdatas/sepgcc";
+    public static final String FILE_BASE = "/data/appdatas/sepgcc-site";
     public static final String[] FILE_EXTENSION_SUPPORT = {
             ".txt", ".pdf",
             ".gif", ".png", ".jpg", ".jpeg", ".bmp",
             ".zip", ".rar", ".7z",
             ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx"
     };
-    public static final int MAX_FILE_SIZE = 1024 * 1024 * 5;
+    public static final int MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
 
     public static final List<Integer> ADMIN_AVAILABLE_PROJECT_STATUS = Lists.newArrayList(0, 1, 2);
     public static final List<Integer> USER_AVAILABLE_PROJECT_STATUS = Lists.newArrayList(1);
